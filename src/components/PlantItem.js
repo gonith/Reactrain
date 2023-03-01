e45f1ea8089dba5b88c8c0fc5568393a3a3d030f
Thibaut Gonzalez <willoz1337@gmail.com>
2023-03-01 16:58:23 +0100

    third commit

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -2,12 +2,13 @@ import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
 function handleClick(plantName) {
-    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
 }
 
-function PlantItem({ cover, name, water, light }) {
+function PlantItem({ cover, name, water, light, price }) {
 	return (
-		<li className='lmj-plant-item' onClick={() => handleClick(name)}>
+		<li className='lmj-plant-item' onClick={() => handleClick}>
+			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
